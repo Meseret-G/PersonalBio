bcf171e1b0437f1f909b5bdb5074bc35632aab3e
Messi_G <negash4@gmail.com>
2022-02-24 23:42:53 -0600

    navbar finalized

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,24 @@ import './intro.scss';
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-      intro
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/logo.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div clasName="wrapper">
+          <h2>Hi there</h2>
+          <h1>Meseret</h1>
+          <h3>
+            Freelance
+            <span />
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/downarrow.png" alt="" />
+        </a>
+      </div>
     </div>
   );
 }
