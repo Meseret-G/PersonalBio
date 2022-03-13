09fc1628f0f33756dbdc4dca12b024a109d6714a
Messi_G <negash4@gmail.com>
2022-03-13 18:08:00 -0500

    portfolio list added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PortfolioList from '../portfolioList/PortfolioList';
 import './portfolio.scss';
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState('featured');
+  const list = [
+    {
+      id: 'featured',
+      title: 'Featured',
+    },
+    {
+      id: 'web',
+      title: 'Web App',
+    },
+    {
+      id: 'mobile',
+      title: 'Mobile App',
+    },
+  ];
   return (
     <div className="portfolio" id="portfolio">
-      Portfolio
+      <h1> Portfolio</h1>
+      <ul>
+        {' '}
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQy9LSUzRzVbHXkXNjPPLAWGNo6yTxjG1Mzg&usqp=CAU"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQy9LSUzRzVbHXkXNjPPLAWGNo6yTxjG1Mzg&usqp=CAU"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQy9LSUzRzVbHXkXNjPPLAWGNo6yTxjG1Mzg&usqp=CAU"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQy9LSUzRzVbHXkXNjPPLAWGNo6yTxjG1Mzg&usqp=CAU"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+      </div>
     </div>
   );
 }
